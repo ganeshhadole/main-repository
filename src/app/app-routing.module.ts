6cea3c04f6fb39d3ffadaa3329b80cae75473ebd
Shivlila-AugmentedAi <shivlila.gandage@augmentedai.ai>
2025-02-13 15:33:15 +0530

    added adim page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { UserChatbotComponent } from './user-chatbot/user-chatbot.component';
 
 const routes: Routes = [
   {
@@ -10,6 +12,14 @@ const routes: Routes = [
   {
     path:'login',
     component:LoginPageComponent
+  },
+  {
+    path:'admin-dashboard',
+    component:AdminDashboardComponent
+  },
+  {
+    path:'user-chatbot',
+    component:UserChatbotComponent
   }
 ];
 
---
+++ src/app/user-chatbot/user-chatbot.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-chatbot',
+  templateUrl: './user-chatbot.component.html',
+  styleUrls: ['./user-chatbot.component.scss']
+})
+export class UserChatbotComponent {
+
+}
